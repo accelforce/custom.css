f0ed674981487fa03c8e6e0b1b3632c998f85252
kyori19 <kyori.dis@gmail.com>
2019-08-10 19:21:01 +0900

    generate.rb

---
+++ generate.rb
@@ -0,0 +1,22 @@
+require "csv"
+
+puts("== custom.css Generator ==")
+
+data = CSV.read("./instances.csv", headers: true)
+
+File.open("./custom.css", "w") do |file|
+    data.each do |line|
+        puts("[#{line["name"]}] #{line["domain"]}")
+        file.write(".status a[href^='https://#{line["domain"]}/'] {\n")
+        file.write("  border-bottom: solid 1px ##{line["bgcolor"]} ;\n")
+        file.write("}\n")
+        file.write(".status a[href^='https://#{line["domain"]}/']\n")
+        file.write(".display-name:before {\n")
+        file.write("  color: ##{line["textcolor"]} !important ;\n")
+        file.write("  background-color: ##{line["bgcolor"]} !important ;\n")
+        file.write("  content: '[#{line["name"]}]' !important ;\n")
+        file.write("}\n")
+    end
+end
+
+puts("== END ==")
